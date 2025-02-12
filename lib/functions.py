6a88e5ad1394f5965a143118b1f40125b934ffb9
Stiveys <stevomwaura@gmail.com>
2025-02-12 09:12:39 +0300

    new

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,32 @@
-#!/usr/bin/env python3
+# lib/functions.py
 
 def greet_programmer():
-    pass
+    """
+    This function takes no arguments and prints "Hello, programmer!" to the terminal.
+    """
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    """
+    This function takes one argument, a name, and prints "Hello, name!" to the terminal.
+    """
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    """
+    This function takes one argument, a name, and prints "Hello, name!" to the terminal.
+    If no argument is passed, it defaults to "programmer".
+    """
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    """
+    This function takes two numbers as arguments and returns their sum.
+    """
+    return num1 + num2
 
 def halve(number):
-    pass
+    """
+    This function takes one number as an argument and returns the number divided by two.
+    """
+    return number / 2
